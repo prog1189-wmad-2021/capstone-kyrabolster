0cc59af8163a861910b1bb0d1dc502192d838e47
kyrabolster <49887176+kyrabolster@users.noreply.github.com>
2021-04-29 14:08:48 -0300

    API created.

---
+++ VastVoyages/VastVoyages.API/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VastVoyages.API.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ VastVoyages/VastVoyages.API/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VastVoyages.Model;
+using VastVoyages.Model.Entities;
+using VastVoyages.Service;
+
+namespace VastVoyages.API.Controllers
+{
+    [RoutePrefix("api/employees")]
+    public class EmployeesController : ApiController
+    {
+        EmployeeService service = new EmployeeService();
+
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            //List<Artist> artists = service.GetAll();
+            //return Ok(artists);
+
+            try
+            {
+                List<EmployeeDTO> employees = service.GetAllEmployees();
+
+                return Ok(employees);
+
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.InternalServerError,
+                    "An internal error has occured. Please contact the system administrator");
+            }
+        }
+    }
+}
